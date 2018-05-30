a9d1d56e8aa8570259aa882dad36faa8824ac839
zxzxzxzxzxtb <306578195@qq.com>
2018-05-31 07:37:05 +0800

    添加项目文件。

---
+++ Instart.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Instart.Web.MvcApplication" Language="C#" %>
---
+++ Instart.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Instart.Web.Startup))]
+namespace Instart.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
