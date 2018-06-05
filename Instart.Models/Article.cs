1e07f26f81beaf4907cf6b38e75c393b27b2130f
liufei <liufei@sodao.com>
2018-06-05 14:33:19 +0800

    增加category和article

---
+++ Instart.Models/Article.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instart.Models
+{
+    public class Article
+    {
+        /// <summary>
+        /// 主键ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 分类ID
+        /// </summary>
+        public int CategoryId { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 副标题
+        /// </summary>
+        public string SubTitle { get; set; }
+
+        /// <summary>
+        /// 作者
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// 文件来源
+        /// </summary>
+        public string Source { get; set; }
+
+        /// <summary>
+        /// 概要
+        /// </summary>
+        public string Summary { get; set; }
+
+        /// <summary>
+        /// 文件内容
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// 是否删除
+        /// </summary>
+        public bool IsDelete { get; set; }
+
+        /// <summary>
+        /// 是否推荐
+        /// </summary>
+        public bool IsReommend { get; set; }
+
+        /// <summary>
+        /// 是否置顶
+        /// </summary>
+        public bool IsTop { get; set; }
+
+        /// <summary>
+        /// 排序
+        /// </summary>
+        public int GroupIndex { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime? ModifyTime { get; set; }
+    }
+}
---
+++ Instart.Models/Category.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instart.Models
+{
+    public class Category
+    {
+        /// <summary>
+        /// 主键ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 父分类ID
+        /// </summary>
+        public int ParentId { get; set; }
+
+        /// <summary>
+        /// 分类名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 分类编码
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// 分类排序
+        /// </summary>
+        public int GroupIndex { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime? ModifyTime { get; set; }
+    }
+}
