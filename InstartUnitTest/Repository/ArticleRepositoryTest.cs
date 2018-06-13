fa00b450fafffd76fb02c69b1978f1aa0f216161
liufei <liufei@sodao.com>
2018-06-13 13:47:41 +0800

    用户表单元测试

---
+++ InstartUnitTest/Repository/ArticleRepositoryTest.cs
@@ -0,0 +1,15 @@
+﻿using Instart.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Instart.UnitTest.Repository
+{
+    public class ArticleRepositoryTest
+    {
+        IArticleRepository _articleRepository = AutofacRepository.Resolve<IArticleRepository>();
+    }
+}
--- InstartUnitTest/Repository/UserRepositoryTest.cs
+++ InstartUnitTest/Repository/UserRepositoryTest.cs
@@ -18,5 +18,48 @@ namespace Instart.UnitTest.Repository
             var user = await _userRepository.GetByIdAsync(1);
             Assert.NotNull(user);
         }
+
+        [Fact]
+        public async Task GetByName_Test()
+        {
+            var user = await _userRepository.GetByNameAsync("admin");
+            Assert.NotNull(user);
+        }
+
+        [Fact]
+        public async Task GetList_Test()
+        {
+            var user = await _userRepository.GetListAsync(1, 10);
+            Assert.NotNull(user);
+        }
+
+        [Fact]
+        public async Task Insert_Test()
+        {
+            var result = await _userRepository.InsertAsync(new Models.User
+            {
+                UserName = "test",
+                Password = "123456",
+                Role = 1,
+                Status = 1,
+                CreateTime = DateTime.Now,
+                ModifyTime = DateTime.Now
+            });
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task UpdatePassword_Test()
+        {
+            var result = await _userRepository.UpdatePasswordAsync(2, "666666");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task Delete_Test()
+        {
+            var result = await _userRepository.DeleteAsync(2);
+            Assert.True(result);
+        }
     }
 }
