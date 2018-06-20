543357d2d40ab5fbec41e3ae85141388b1fdd1e6
liufei <liufei@sodao.com>
2018-06-20 19:01:47 +0800

    增加automapper

---
+++ Instart.Web/Extensions/AutoMapperExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Instart.Web
+{
+    public static class AutoMapperExtensions
+    {
+        /// <summary>
+        /// 泛型映射
+        /// </summary>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static TDestination Mapper<TDestination>(this object source)
+        {
+            return AutoMapper.Mapper.Map<TDestination>(source);
+        }
+    }
+}
