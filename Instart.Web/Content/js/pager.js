3898ddbf0914d4f036907aee564a97155b5c8771
zxzxzxzxzxtb <306578195@qq.com>
2018-07-20 00:18:19 +0800

    分页组件

---
+++ Instart.Web/Content/js/pager.js
@@ -0,0 +1,39 @@
+﻿function getPageBar(curPage, totalPage, $container) {
+    $container.find('*').remove();
+
+    if (totalPage <= 1) {
+        return;
+    }
+
+    if (curPage > totalPage) {
+        curPage = totalPage;
+    }
+
+    if (curPage < 1) {
+        curPage = 1;
+    }
+
+    var pageStr = '<ul>';
+    if (curPage == 1) {
+        pageStr += "<li class='pre'><a>上一页</a></li>";
+    } else {
+        pageStr += "<li class='pre'><a href='javascript:void(0)' rel='" + (curPage - 1) + "'>上一页</a></span>";
+    }
+
+    for (var i = 1; i <= totalPage; i++) {
+        if (i == curPage) {
+            pageStr += '<li class="active"><a href="javascript:;" rel="' + i + '">' + i + '</a></li>'
+        }
+        else {
+            pageStr += '<li><a href="javascript:;" rel="' + i + '">' + i + '</a></li>'
+        }
+    }
+    if (curPage >= totalPage) {
+        pageStr += "<li class='next'><a>下一页</a></li>";
+    } else {
+        pageStr += "<li class='next'><a href='javascript:void(0)' rel='" + (parseInt(curPage) + 1) + "'>下一页</a></li>";
+    }
+    pageStr += '</ul>';
+
+    $container.append(pageStr);
+}
--- Instart.Web/Controllers/TeacherController.cs
+++ Instart.Web/Controllers/TeacherController.cs
@@ -25,36 +25,37 @@ namespace Instart.Web.Controllers
             this.AddDisposableObject(_bannerService);
         }
 
-        public async Task<ActionResult> Index(int divisionId = 0)
+        public async Task<ActionResult> Index(int id = 0)
         {
             var divisionList = await _divisionService.GetAllAsync();
+
             if (divisionList == null || divisionList.Count() == 0)
             {
                 throw new Exception("请先创建学部");
             }
 
-            if(divisionId == 0)
+            if(id == 0)
             {
-                divisionId = divisionList.First().Id;
+                id = divisionList.First().Id;
             }
 
-            var teacherList = await _teacherService.GetListByDivsionAsync(divisionId, 1, 8);
-
             ViewBag.DivisionList = divisionList;
-            ViewBag.DivisionId = divisionId;
-            ViewBag.Total = teacherList.Total;
-            ViewBag.TotalPages = (int)Math.Ceiling(teacherList.Total * 1.0 / 8);
-            ViewBag.TeacherList = teacherList.Data ?? new List<Instart.Models.Teacher>();
+            ViewBag.DivisionId = id;
             ViewBag.BannerList = (await _bannerService.GetBannerListByPosAsync(Instart.Models.Enums.EnumBannerPos.Teacher)) ?? new List<Instart.Models.Banner>();
-
             return View();
         }
 
-        [HttpGet]
+        [HttpPost]
         public async Task<JsonResult> GetTeacherList(int divisionId, int pageIndex, int pageSize = 8)
         {
-            var teacherList = await _teacherService.GetListByDivsionAsync(divisionId, pageIndex, pageSize);
-            return Success(data: teacherList);
+            var result = await _teacherService.GetListByDivsionAsync(divisionId, pageIndex, pageSize);
+            return Success(data: new
+            {
+                total = result.Total,
+                pageSize = pageSize,
+                totalPage = (int)Math.Ceiling(result.Total * 1.0 / pageSize),
+                list = result.Data
+            });
         }
 
         public async Task<ActionResult> Details(int id)
