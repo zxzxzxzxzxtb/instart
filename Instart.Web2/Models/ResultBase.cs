4df99c2ce9a0f8c6fb38ca9d02f344f110bd473b
zxzxzxzxzxtb <306578195@qq.com>
2018-07-29 18:54:43 +0800

    models

---
+++ Instart.Web2/Models/ResultBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Instart.Web2.Models
+{
+    public class ResultBase
+    {
+        public bool success { get; set; }
+
+        public string message { get; set; }
+
+        public int errorcode { get; set; }
+
+        public object data { get; set; }
+    }
+}
