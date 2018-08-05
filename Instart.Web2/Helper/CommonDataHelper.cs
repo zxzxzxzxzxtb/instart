aafc6575bf77613f49e83bdb0eef81465fdedd35
zxzxzxzxzxtb <306578195@qq.com>
2018-08-05 19:32:11 +0800

    nothing

---
+++ Instart.Web2/Helper/CommonDataHelper.cs
@@ -0,0 +1,31 @@
+﻿using Instart.Models;
+using Instart.Models.Enums;
+using Instart.Service;
+using Instart.Service.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Instart.Web2.Helper
+{
+    public class CommonDataHelper
+    {
+        public static List<Banner> GetBannerList(EnumBannerPos pos)
+        {
+            var list = AutofacService.Resolve<IBannerService>().GetBannerListByPosAsync(pos);
+            if (list != null && list.Count > 0)
+            {
+                return list;
+            }
+
+            list = AutofacService.Resolve<IBannerService>().GetBannerListByPosAsync(EnumBannerPos.Index);   // 如果页面没有banner配置，默认给index的banner配置
+            return list ?? new List<Instart.Models.Banner>();
+        }
+
+        public static List<Course> GetCourseList()
+        {
+            return (AutofacService.Resolve<ICourseService>().GetRecommendListAsync(3)) ?? new List<Instart.Models.Course>();
+        }
+    }
+}
