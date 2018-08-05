a9add42cea8e5c45010d12c8076a410951f70695
zxzxzxzxzxtb <306578195@qq.com>
2018-08-05 20:07:12 +0800

    nothing

--- Instart.Web2/Controllers/AboutController.cs
+++ Instart.Web2/Controllers/AboutController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using Instart.Models;
 using Instart.Service;
 using Instart.Service.Base;
+using Instart.Models.Enums;
 
 namespace Instart.Web2.Controllers
 {
@@ -17,6 +18,9 @@ namespace Instart.Web2.Controllers
     {
         IAboutInstartService _aboutService = AutofacService.Resolve<IAboutInstartService>();
         IBannerService _bannerService = AutofacService.Resolve<IBannerService>();
+        IProgramService _programService = AutofacService.Resolve<IProgramService>();
+        ICompanyService _companyService = AutofacService.Resolve<ICompanyService>();
+        IStudioService _studioService = AutofacService.Resolve<IStudioService>();
 
         public AboutController()
         {
@@ -34,6 +38,62 @@ namespace Instart.Web2.Controllers
                 ViewBag.BannerUrl = bannerList[0].ImageUrl;
             }
             return View(model);
+        }
+
+        public ActionResult program(EnumProgramType type)
+        {
+            var list = _programService.GetListByTypeAsync((int)type) ?? new List<Program>();
+            ViewBag.Type = type.ToString();
+            return View(list);
+        }
+
+        public ActionResult Show(int id)
+        {
+            var detail = _programService.GetByIdAsync(id);
+            if (detail == null)
+            {
+                throw new Exception("内容不存在");
+            }
+
+            ViewBag.Type = detail.Type.ToString();
+            return View(detail);
+        }
+
+        public ActionResult Division()
+        {
+            ViewBag.Type = "Division";
+            return View();
+        }
+
+        public ActionResult Company()
+        {
+            ViewBag.Type = "Company";
+            var list = _companyService.GetAllAsync();
+            return View(list);
+        }
+
+        public ActionResult CompanyDetail(int id)
+        {
+            var model = _companyService.GetByIdAsync(id);
+            if (model == null)
+            {
+                throw new Exception("内容不存在");
+            }
+            ViewBag.Type = "Company";
+            return View(model);
+        }
+
+        public ActionResult Studio()
+        {
+            ViewBag.Type = "Studio";
+            var model = _studioService.GetInfoAsync();
+            return View(model);
+        }
+
+        public ActionResult Teach()
+        {
+            ViewBag.Type = "Teach";
+            return View();
         }
     }
 }
